3bba4ee1e03c92ff607ac7c3b426b3585c76b9f9
Chin Yang Oh <iamohcy@gmail.com>
2013-10-20 16:49:46 -0400

    fixed bugs in kernel.c -ohcy

--- lab2/part2/kernel/kernel.c
+++ lab2/part2/kernel/kernel.c
@@ -14,7 +14,7 @@
 #define SWI_VEC_ADDR    0x8
 #define LDR_OPCODE_UP   0xe59ff000
 #define LDR_OPCODE_DOWN 0xe51ff000
-#define LDR_IMM_MASK    0xfffff000
+#define LDR_IMM_MASK    0x00000fff
 #define USR_STACK_START 0xa3000000
 #define USR_PROG_ENTRY  0xa2000000
 
@@ -25,32 +25,32 @@ int main(int argc, char *argv[]) {
     /*
      * Install SWI handler
      */
-    
+
     // Obtain SWI vector.
-    int swi_vector = *SWI_VECTOR_ADDR;
-    
+    int swi_vector = *SWI_VEC_ADDR;
+
     // Check that SWI vector is valid and exit if not.
-    int vec_instr = swi_vector & ~SWI_OFFSET_MASK;
-    if (vec_instr != LDR_OPCODE_UP || vec_instr != LDR_OPCODE_DOWN) {
+    int swi_vector_instr = swi_vector & ~LDR_IMM_MASK;
+    if (swi_vector_instr != LDR_OPCODE_UP || swi_vector_instr != LDR_OPCODE_DOWN) {
         puts("Unrecognized SWI Vector!\n");
         return 0x0badc0de;
     }
-    
+
     // Extract the LDR immediate load U-Boot's SWI handler addr.
-    int sign = (vec_instr == LDR_OPCODE_UP) ? 1 : -1;
-    int vec_imm = (vec_instr & LDR_IMM_MASK) * sign;
-    int *swi_handler = SWI_VEC_ADDR + PREFETCH_OFFSET + vec_imm;
-    
+    int sign = (swi_vector_instr == LDR_OPCODE_UP) ? 1 : -1;
+    int swi_vec_imm = (swi_vector & LDR_IMM_MASK) * sign;
+    int *swi_handler = SWI_VEC_ADDR + PREFETCH_OFFSET + swi_vec_imm;
+
     // Save U-Boot SWI handler into global var.
     uboot_swi_handler = *swi_handler;
-    
+
     // Load our own SWI handler.
     *swi_handler = (LDR_OPCODE_DOWN | 0x04);
     *(swi_handler + 1) = &swi_handler;
-    
+
     // Load user program and exit after user program is done.
     asm("msr CPSR_c, #0xD0\n\t"); // Switch to user mode.
     // TODO XXX
-    
+
     return 0; // To satisfy GCC
 }
