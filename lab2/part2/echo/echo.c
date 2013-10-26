21a06db639d2ccddba2247b3f9b3f95ba2b37396
kuanweeloong <weeloong@cmu.edu>
2013-10-25 21:32:26 -0400

    Fixed potential bug with echo not fully writing the string to stdout.

--- lab2/part2/echo/echo.c
+++ lab2/part2/echo/echo.c
@@ -7,7 +7,20 @@
  * Date:    20 Oct 2013
  */
 
+#include <stdlib.h>
 #include <unistd.h>
+
+// Writes a string to stdout.
+void write_string(char *str, unsigned int len) {
+    int write_size = 0;
+    
+    do {
+        if ((write_size = write(STDOUT_FILENO, str, len)) < 0) {
+            exit(1);
+        }
+        len -= write_size;
+    } while (len > 0);
+}
 
 int main(int argc, char** argv) {
     int i = 1;
@@ -20,14 +33,15 @@ int main(int argc, char** argv) {
             arg_len++;
         }
         
-        write(STDOUT_FILENO, argv[i], arg_len);
-        write(STDOUT_FILENO, " ", 1);
+        // Write argument and space to stdout.
+        write_string(argv[i], arg_len);
+        write_string(" ", 1);
         
         i++;
     }
     
-    // Write a newline
-    write(STDOUT_FILENO, "\n", 1);
+    // Write a newline before exiting.
+    write_string("\n", 1);
     
     return 0;
 }
