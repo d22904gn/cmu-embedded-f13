5e6ca573a1140a0b75d3759f0c2d0400e74b4558
kuanweeloong <kuanweeloong@server.fake>
2013-11-08 00:07:23 -0500

    Spinning cursor: timing a little off

--- lab3/tasks/splat/splat.c
+++ lab3/tasks/splat/splat.c
@@ -3,10 +3,40 @@
  * @brief Displays a spinning cursor.
  *
  * Links to libc.
+ * @author Wee Long Kuan
+ * @author Chin Yang Oh
+ * @author Jennifer Lee
  */
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 
 int main(int argc, char** argv)
 {
-	/* Add code here -- put your names at the top. */
+    int i = 0;
+    // Symbols for turning cursor
+    int sym_size = 4;
+    char sym_arr[sym_size];
+    sym_arr[0] = '|';
+    sym_arr[1] = '/';
+    sym_arr[2] = '-';
+    sym_arr[3] = '\\';
+    
+    // Overwritten Element on first iteration
+    printf("%c",sym_arr[0]);
+    
+    while(1) {
+        // Move cursor backward and write next char
+        printf("\b%c",sym_arr[i]);
+        i++;
+        if (i >= sym_size) {
+            i = i % sym_size;
+        }
+        sleep(200);
+    }
+    
 	return 0;
 }
+
