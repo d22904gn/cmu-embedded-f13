cfb4283e1d8093f5c461eeb1b8901875dfb3b7bc
kuanweeloong <kuanweeloong@server.fake>
2013-11-10 14:29:10 -0500

    SWI handler bugfix. Some time app stuff.

--- lab3/tasks/time/time.c
+++ lab3/tasks/time/time.c
@@ -1,13 +1,13 @@
 #include <unistd.h>
 #include <stdio.h>
-#include <string.h>
 
 int main(int argc, char** argv)
 {
-    printf("start timing!\n");
-    
-    sleep(10000);
-    printf("new time is: %lu\n", time());
+    printf("Curr time is: %lu.\nNow sleeping for 2000ms...\n", time());
+    sleep(2000);
+    printf("Curr time is: %lu.\n Now sleeping for 15 minutes...\n", time());
+    sleep(900000);
+    printf("Done! Curr time is: %lu\n", time());
     
     return 0;
 }
--- utils/start.sh
+++ utils/start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-
+cat gumstix_cmds | xclip -selection c
 qemu-system-arm -nographic -M verdex -pflash flash.img -sd sdcard.img -s
