3adb109154ad056f8d3c17d9ee87b048f938b221
kuanweeloong <kuanweeloong@server.fake>
2013-11-10 19:13:22 -0500

    IRQ handler bugfix with not restoring old registers. Some cleanup code for both handlers. Time syscall bugfix with wrong millisecond factor.

--- lab3/kernel/syscalls/time.c
+++ lab3/kernel/syscalls/time.c
@@ -9,14 +9,15 @@
  * @date    06 Nov 2013
  */
 
+#include <types.h>
 #include <arm/reg.h>
 #include <arm/timer.h>
 #include "../interrupts/timers.h"
 
 // MATH!
-#define MS_PER_HOUR 3600000u
+#define MS_PER_OVERFLOW ((uint32_t) ((UINT_MAX / OSTMR_FREQ) * 1000u))
 
 unsigned long time() {
-    return (clock_overflows * MS_PER_HOUR) +
+    return (clock_overflows * MS_PER_OVERFLOW) +
            get_ms(reg_read(OSTMR_OSCR_ADDR));
 }
--- lab3/tasks/time/time.c
+++ lab3/tasks/time/time.c
@@ -5,7 +5,7 @@ int main(int argc, char** argv)
 {
     printf("Curr time is: %lu.\nNow sleeping for 2000ms...\n", time());
     sleep(2000);
-    printf("Curr time is: %lu.\n Now sleeping for 15 minutes...\n", time());
+    printf("Curr time is: %lu.\nNow sleeping for 15 minutes...\n", time());
     sleep(900000);
     printf("Done! Curr time is: %lu\n", time());
     
